6d50d6333ad35066b6aabeda977823b01f2a4eac
Alexis Sanehisa <asanehisa@yext.com>
2024-03-12 14:10:09 -0400

    add link to analytics

--- src/modules/analytics/potato.tsx
+++ src/modules/analytics/potato.tsx
@@ -1,5 +1,5 @@
 import { Module, ModuleConfig } from "@yext/pages/*";
-import { AnalyticsProvider } from "@yext/pages-components";
+import { AnalyticsProvider, Link } from "@yext/pages-components";
 import "./index.css";
 
 export const config: ModuleConfig = {
@@ -8,8 +8,12 @@ export const config: ModuleConfig = {
 
 const analytics: Module = () => {
   return(
-    <AnalyticsProvider templateData={{document}}>
+    <AnalyticsProvider templateData={{document}} enableDebugging={true}>
       <div className="tailwind">
+        Analytics 
+        <Link href="https://cdn.mos.cms.futurecdn.net/iC7HBvohbJqExqvbKcV3pP-1200-80.jpg.webp">
+          Test Link
+        </Link>
       </div>
     </AnalyticsProvider>
   )
